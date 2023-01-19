75ce24dc3c1f0c716b39ba191fa066e3e524970d
michaelperezchavira <michaelperezchavira@LEM231-28>
2023-01-18 18:56:15 -0800

    Add project files.

---
+++ COMP003A.LectureActivity1/Program.cs
@@ -0,0 +1,22 @@
+﻿// this is a single line comment
+/*
+ * Author: Michael Perez Chavira
+ * Course: COMP-003A
+ * Purpose: Lecture activity for this week
+ */
+
+using System.Runtime.CompilerServices;
+
+namespace COMP003A.LectureActivity1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); // add comments here too
+            Console.WriteLine("this is a string");
+            Console.WriteLine("Michael Perez Chavira");
+
+        }
+    }
+}
